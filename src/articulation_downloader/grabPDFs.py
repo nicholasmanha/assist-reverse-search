df42c58c248d5a97789c7cca5ed23ecfd4060b25
Nicholas Manha <nickrmanha@gmail.com>
2024-11-05 19:10:45 -0800

    grabs college id's of colleges with agreements with target college

--- src/articulation_downloader/grabPDFs.py
+++ src/articulation_downloader/grabPDFs.py
@@ -1,4 +1,5 @@
 import urllib, json
+from urllib.error import HTTPError
 
 def grabPDFs():
     institutions_url = urllib.request.urlopen("https://assist.org/api/institutions")
@@ -11,3 +12,13 @@ def grabPDFs():
         if curr_college == target_college:
             id = institutions[entry]['id']
             break
+    try:
+        institution_agreements = urllib.request.urlopen("https://assist.org/api/institutions/"+ str(id) + "/agreements")
+        agreement_data = json.load(institution_agreements)
+        colleges = [] # list of all college id's that have an agreement with the target college
+        for entry in range(len(agreement_data)):
+            colleges.append(agreement_data[entry]['institutionParentId'])
+
+    except HTTPError as e:
+        content = e.read()
+        print()
