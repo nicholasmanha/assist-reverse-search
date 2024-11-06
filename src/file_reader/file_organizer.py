947f775f0e31d19a39475b81f3f4fae5b4b0b4b0
Nicholas Manha <nickrmanha@gmail.com>
2024-11-05 23:05:33 -0800

    Added function to get course list

---
+++ src/file_reader/file_organizer.py
@@ -0,0 +1,23 @@
+from os.path import isfile, join
+import os
+from pathlib import Path
+
+def courseList():
+    ROOT_DIR = os.path.dirname(os.path.abspath("main.py"))
+    output_path = os.path.join(ROOT_DIR, 'articulation_downloader', 'outputs')
+
+    college_list = []
+    # all files in directory
+    files = [f for f in os.listdir(output_path) if isfile(join(output_path, f))]
+
+    for file in files:
+        col = isCourseArticulated(file)
+        if col is not None:
+            college_list.append(col)
+
+    return college_list
+
+def isCourseArticulated(file):
+    ROOT_DIR = os.path.dirname(os.path.abspath("main.py"))
+    output_path = os.path.join(ROOT_DIR, 'articulation_downloader', 'outputs')
+    
