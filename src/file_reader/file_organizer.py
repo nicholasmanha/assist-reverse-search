d41efb17612749c68807dd7b4998b06e2f79cc8a
Nicholas Manha <nickrmanha@gmail.com>
2025-01-13 09:36:50 -0800

    functions take arguments from click

--- src/file_reader/file_organizer.py
+++ src/file_reader/file_organizer.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pymupdf
 
 
-def courseList():
+def courseList(course):
     ROOT_DIR = os.path.dirname(os.path.abspath("main.py"))
     output_path = os.path.join(ROOT_DIR, 'articulation_downloader', 'outputs')
 
@@ -14,18 +14,18 @@ def courseList():
         output_path) if isfile(join(output_path, f))]
 
     for file in files:
-        col = isCourseArticulated(file)
+        col = isCourseArticulated(file, course)
         if col is not None:
             college_list.append(col)
 
     return college_list
 
 
-def isCourseArticulated(file):
+def isCourseArticulated(file, course):
     ROOT_DIR = os.path.dirname(os.path.abspath("main.py"))
     doc = pymupdf.open("articulation_downloader/outputs/" + file)  # open a document
     out = open("output.txt", "wb")  # create a text output
-    search_term = "MATH​ 107"
+    search_term = "MATH​ 54"
     for page_number in range(len(doc)):  # iterate the document pages via page number
         page = doc.load_page(page_number)
         college = ""
@@ -33,11 +33,11 @@ def isCourseArticulated(file):
         if page_number == 0: # get the name of the college via the first page
             college = text[text.find('From:') + 6:text.find('2', text.find('From:')) - 1] # format: "From: [college] 2021-2022"
         textOutput = page.get_text().encode("utf8") # get plain text (is in UTF-8) (bytes)
-        search_position = text.find(search_term)
+        search_position = text.find(course)
         out.write(textOutput)
         out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
         if search_position != -1: 
-            arrow_index = text.find("←", search_position + len(search_term))
+            arrow_index = text.find("←", search_position + len(course))
             articulated_course = text[arrow_index:arrow_index + 30]
             if "No Course Articulated" not in articulated_course and \
                 "No Comparable Course" not in articulated_course and \
