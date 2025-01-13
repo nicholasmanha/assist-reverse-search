e3776e5b4ce03a2c8ca4ba42629c064006d32324
Nicholas Manha <nickrmanha@gmail.com>
2025-01-13 09:52:32 -0800

    removed debug

--- src/file_reader/file_organizer.py
+++ src/file_reader/file_organizer.py
@@ -22,35 +22,25 @@ def courseList(course):
 
 
 def isCourseArticulated(file, course):
-    ROOT_DIR = os.path.dirname(os.path.abspath("main.py"))
-    doc = pymupdf.open("articulation_downloader/outputs/" + file)  # open a document
-    out = open("output.txt", "wb")  # create a text output
+    doc = pymupdf.open("articulation_downloader/outputs/" + file)  # open the agreement
     college = ""
-    for page_number in range(len(doc)):  # iterate the document pages via page number
-        page = doc.load_page(page_number)
-        
+    for page in doc:  # iterate through the document pages
         text = page.get_text() 
-        if text.find('From:') != -1:
+        if text.find('From:') != -1: # if the page doesn't have "From:", then the college title isn't on that page
             college = text[text.find('From:') + 6:text.find('2', text.find('From:')) - 1] # format: "From: [college] 2021-2022"
-        
-        textOutput = page.get_text().encode("utf8") # get plain text (is in UTF-8) (bytes)
         search_position = text.find(course)
-        out.write(textOutput)
-        out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
         if search_position != -1: 
-            arrow_index = text.find("←", search_position + len(course))
+            arrow_index = text.find("←", search_position + len(course)) # look after first arrow after the course
             articulated_course = text[arrow_index:arrow_index + 30]
+            # if the course is articulated
             if "No Course Articulated" not in articulated_course and \
                 "No Comparable Course" not in articulated_course and \
                     "Course(s) Denied" not in articulated_course:
                 doc.close()
-                print(college)
                 return college
             else:
                 doc.close()
                 os.remove("articulation_downloader/outputs/" + file)
                 return
-        
-    out.close()
             
     
