19d1cf7f5ad1ef9ad29504d0c234d4c0c5c8821b
Nicholas Manha <nickrmanha@gmail.com>
2025-01-13 09:29:00 -0800

    main integrates click and gets command args

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
 from file_reader.file_organizer import courseList
 from articulation_downloader.grab_PDFs import grabPDFs
+import click
 
-grabPDFs()
-lis = courseList()
-print(lis)
-print(len(lis))
+@click.command()
+@click.option('--university', prompt='Univerity',
+              help='The University you wish to attend.')
+@click.option('--degree', prompt='Degree',
+              help='The degree you wish to look at.')
+@click.option('--course', prompt='Course',
+              help='The course you wish to take.')
+
+def find_colleges(university, degree, course):
+  grabPDFs(university, degree)
+  lis = courseList(course)
+  print(lis)
+  print(len(lis))
+        
+if __name__ == '__main__':
+    find_colleges()
